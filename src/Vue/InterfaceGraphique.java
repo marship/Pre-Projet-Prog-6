e77bf725f2b3147493a292a54042a01011a1303a
Aglemba <94596458+Aglemba@users.noreply.github.com>
2022-04-29 13:59:46 +0200

    Commit Conflit

--- src/Vue/InterfaceGraphique.java
+++ src/Vue/InterfaceGraphique.java
@@ -22,14 +22,9 @@ public class InterfaceGraphique implements Runnable, Observateur {
     boolean estMaximise;
     JFrame frame;
     public GaufreGraphique gaufreGraphique;
-<<<<<<< Updated upstream
-    JLabel nbPas, nbPoussees, infoJoueurCourant, infoFin, scores, J1L, J2L;
+    JLabel infoJoueurCourant, infoFin, scores, J1L, J2L;
     JButton annuler, refaire, nouvellePartie, suite;
     int J1, J2;
-=======
-    JLabel infoJoueurCourant, infoFin;
-    JButton annuler, refaire;
->>>>>>> Stashed changes
 
     InterfaceGraphique(Jeu j, CollecteurEvenements cEvenements) {
         jeu = j;
@@ -117,7 +112,6 @@ public class InterfaceGraphique implements Runnable, Observateur {
         frame.setVisible(true);
     }
 
-<<<<<<< Updated upstream
     public void majScore(){
         if(J1 > J2){
             J1L.setForeground(new Color(104, 186, 118));
@@ -135,11 +129,11 @@ public class InterfaceGraphique implements Runnable, Observateur {
         }
         J1L.setText("J1 : " + J1);
         J2L.setText("    J2 : " + J2);
-=======
+    }
+
     public void previsualisation(int joueurCourant, int coupX, int coupY, int largeurPreselection, int hauteurPreselection) {
         gaufreGraphique.tracerRectangle(joueurCourant, coupX, coupY, largeurPreselection, hauteurPreselection);
         ((Component) gaufreGraphique).repaint();
->>>>>>> Stashed changes
     }
 
     @Override
